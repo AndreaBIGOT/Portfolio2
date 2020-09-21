996435f44253e438ec300979ac5645f84c0c6cef
Andréa <70588437+AndreaBIGOT@users.noreply.github.com>
2020-09-21 12:47:56 +0400

    Initial commit

---
+++ accueil.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/accueil.css" /> <!--changer police-->
+		<link rel="icon" href="media/iconSite.ico" />
+		<meta charset="utf-8">
+		<title>Portfolio Andréa BIGOT </title>
+	</head>
+	<body>
+
+		<!--bloc image présentation-->
+		<div class="bgimg-1">
+			<div class="caption">
+                <span class="border">Andréa BIGOT</span>
+                <p style="margin: auto;">Bienvenue sur mon portfolio</p>
+		  	</div>
+		</div>
+		
+		
+		<!--menu-->
+        <div style="color: #777;background-color:white;text-align:center;padding:20px 80px;text-align: justify;">
+				<div class="menu">
+					<a href="#moi">    A propos	   </a>
+					<a href="#skill">  Compétences </a>
+                    <a href="#real">   Réalisations</a>
+                    <a href="#service">Services	   </a>
+                    <a href="#contact">Contact	   </a>
+				</div>
+		</div>
+
+		<!--bloc  image à propos-->
+		<div class="bgimg-2" id="moi" >
+            <!-- <div style=" background-color: black; font-size: 25px; color: #f7f7f7; font-style: bold; padding: 18px; position: inherit; letter-spacing: 10px; width: 270px; margin: auto; margin-bottom: 20px;">Qui suis-je ?</div> -->
+            <div class="identity">
+                <div class="left">
+                    <p>Je m’appelle Andréa BIGOT et je suis actuellement en première année de BTS SIO (Services Informatiques aux Organisation). L’informatique est un domaine très vaste qui m’intrigue énormément. C’est pourquoi j’ai choisi cette formation qui me permettra d’acquérir des compétences et savoir-faire solides afin de réaliser mes projets professionnels et personnels.</p>
+                </div>
+                <div class="right">
+                    <img src="media/logoSite3.png" alt="">
+                    <ul>
+                        <li> <h1> Andréa BIGOT </h1> </li>
+						<a href="www.linkedin.com/in/andréa-bigot-10725a1b6"> <li>LinkedIn </li> </a>
+						<a href="https://github.com/AndreaBIGOT">			  <li> Github  </li> </a>
+						<a href="#"> 										  <li>Fiverr   </li> </a>
+						<li> <a href="media/CV.pdf"><input type="submit" value="Voir mon CV" class="bu3tton"> </a> </li><!--CV-->
+						<li> <a href=""><input type="submit" value="Voir mes veilles technologiques" class="bu3tton"> </a> </li><!--VEILLES TECHNO-->
+						<li> <a href=""><input type="submit" value="Voir mes veilles juridiques" class="bu3tton"> </a> </li><!--VEILLES JURIDIQUES-->
+                    </ul>
+                </div>
+			</div>
+		
+			<div id="scrollTop">
+				<a href="#top"> <img src="media/top2.png" alt="Haut de page" title="Haut de page"> </a>
+			</div>
+		</div>
+		
+
+		<!--bloc image compétences-->
+		<div class="bgimg-3" id="skill">
+			<div class="caption">
+		    	<span class="border" style="font-size: 36px; color: #f7f7f7;">Mes compétences</span>
+			</div>
+		</div>
+
+			<div style="position:relative;">
+				<div style="color:#ddd;background-color:rgb(239, 239, 239);padding:50px 80px;text-align: justify;" class="competence">
+					
+					<table>
+						<tr>
+							<th>JAVA</th>
+							<th>MySQL</th>
+							<th>HTML</th>
+							<th>CSS</th>
+							<th>PHP</th>
+						</tr>
+						<tr>
+							<td>Responsive design</td>
+							<td>Validation W3C </td>
+							<td>Photoshop</td>
+							<td>Visual Studio Code</td>
+							<td>Android Studio</td>
+						</tr>
+						<tr>
+							<td>Anglais: niveau B2</td>
+							<td>Français: langue maternelle</td>
+							<td>Espagnol: les bases</td>
+							<td>Créole réunionnais: langue maternelle</td>
+						</tr>
+						<tr>
+							<td>A l'écoute</td>
+							<td>Motivée</td>
+							<td>Travailleuse</td>
+							<td>Perfectionniste</td>
+						</tr>
+
+					</table>
+			  </div>
+			</div>
+
+		<!--bloc image réalisations-->
+		<div class="bgimg-4" id="real">
+		 	<div class="caption">
+		    	<span class="border" style="font-size: 36px; color: #f7f7f7;">Mes réalisations</span>
+			</div>
+		</div>
+
+		<!--bloc des réalisations-->
+		<div class="real">
+			<!--BIRAUTO-->
+			<div class="bloc-img img1">
+				<a href="file:///E:/Birauto/contact.html">	
+				    <div class="overlay">
+	       				<div class="text">Birauto <br/>Projet scolaire <br/> HTML/CSS <br/> Réalisation de page contact <br>et renseignements  
+	       				</div>
+	  				</div>
+	  			</a>
+	  		</div>
+
+			<!--S&D-->
+	  		<div class="bloc-img img2">
+			  	<a href="#" title="Site bientôt disponible !">
+				    <div class="overlay">
+		       			<div class="text">Sweet and Delicious <br/> Projet personnel <br/> HTML/CSS/PHP <br/> Création de mon site de pâtisserie  
+		       			</div>
+	  				</div>
+	  			</a>
+            </div>
+            
+            <!--Cour'ag-->
+			<div class="bloc-img img3">
+				<a href="#" title="Site bientôt disponible !">
+				    <div class="overlay">
+	       				<div class="text">Cour'ag <br/>Projet personnel <br/> HTML/CSS/PHP <br/> Création d'un site de présentation de <br>mon association sportive  
+	       				</div>
+	  				</div>
+	  			</a>
+              </div>
+              
+              <!--MY COOKIES 974-->
+			<div class="bloc-img img4">
+				<a href="#" title="Site bientôt disponible !">	
+				    <div class="overlay">
+	       				<div class="text">My Cookies 974 <br/>Projet personnel <br/> HTML/CSS/PHP <br/> Réalisation d'un site pour une <br> entreprise de vente en ligne  
+	       				</div>
+	  				</div>
+	  			</a>
+              </div>
+              
+              <!--LE POIVRIER-->
+			<div class="bloc-img img5">
+				<a href="http://83.198.215.203/poivrier/">	
+				    <div class="overlay">
+	       				<div class="text">Le Poivrier <br/>Projet d'entraînement <br/> HTML/CSS/PHP <br/> Réalisation d'un site de restauration <br> en ligne  
+	       				</div>
+	  				</div>
+	  			</a>
+	  		</div>
+        </div>
+        
+        <!--bloc image service-->
+		<div class="bgimg-3" id="service">
+			<div class="caption">
+		    	<span class="border" style="font-size: 36px;color: #f7f7f7;">Mes services</span>
+			</div>
+		</div>
+
+			<div style="position:relative;">
+                <div style="color:#ddd;background-color:rgb(239, 239, 239);padding:50px 80px;text-align: justify;">
+					
+					<div class="service">
+						
+						<div class="siteVitrine">
+							<h3>Création de site vitrine</h3>
+							<h5>Developpé sur mesure (codage à la main)</h5>
+							<hr>
+							
+							<ul>
+								<li>HTML CSS PHP</li>
+								<li>Mise en place du contenu (images, vidéos, logo...)</li>
+								<li>2 à 3 pages max</li>
+								<li>Site responsive (adapté tous supports)</li>
+								<li>Liens de partage sur les réseaux sociaux</li>
+								<li>Respect des normes W3C</li>
+								<li>Aide pour la mise en ligne et l'hébergement</li>
+							</ul>
+							<hr>
+							<p>Fonctionnalités au choix:</p>
+							<ul>
+								<li>Système de recherche</li>
+								<li>Formulaire de contact</li>
+								<li>Système de connexion/inscription</li>
+								<li>Pagination</li>
+							</ul>
+						</div>
+
+						<div class="refonte">
+							<h3>Refonte de votre site</h3>
+							<h5>Developpé sur mesure (codage à la main)</h5>
+							<hr>
+							
+							<ul>
+								<li>HTML CSS PHP</li>
+								<li>Refonte graphique</li>
+								<li>Améliorations</li>
+								<li>Ajout de fonctionnalités</li>
+								<li>Cahier des charges sur mesure (ou le votre)</li>
+							</ul>
+							<hr class="hr2">
+							<p>Fonctionnalités au choix:</p>
+							<ul>
+								<li>Système de recherche</li>
+								<li>Formulaire de contact</li>
+								<li>Système de connexion/inscription</li>
+								<li>Pagination</li>
+							</ul>
+						</div> 
+					</div>	
+                </div>
+            </div>
+
+	<!--bloc image contact-->
+		<div class="bgimg-5" id="contact">
+			<div class="caption">
+		    	<span class="borderContact">Contactez-moi</span> 
+			</div>
+		</div>
+
+		<div style="position:relative;">
+			<div style="color:#ddd;background-color:rgb(239, 239, 239);text-align:center;padding:50px 80px 10px 80px;text-align: justify;">
+
+				<?php
+
+					require('connexionBDD.php');
+
+					if (isset($_POST['submit'])) {
+						if (isset($_POST['nom']) && isset($_POST['mail']) && isset($_POST['objet']) && isset($_POST['message'])) {
+
+							$req="INSERT INTO contact(nom, mail, objet, message) VALUES(?,?,?,?)";
+							$insert= $conn->prepare($req);
+							$insert->execute([ $_POST['nom'],$_POST['mail'], $_POST['objet'],$_POST['message'] ]);
+							
+							
+
+							if($insert == true){
+								$msg= "Votre message a bien été envoyé.";
+							}
+
+							else{
+								$msg= "Erreur lors de l'envoi du message. Veuillez réessayer.";
+							}
+						}
+					}
+				?>
+
+
+
+
+				<!--formulaire-->
+				<form method="POST">
+					<input type="text" placeholder="Nom" size="30" maxlength="15" class="champ" name="nom"><br/>
+					<input type="email" placeholder="Adresse mail" size="30" maxlength="15" class="champ" name="mail" /><br/>
+					<input type="text" placeholder="Objet" size="30" maxlength="10" class="champ" name="objet"/><br/>
+					<textarea name="message" placeholder="Votre message" class="champ" name="message"></textarea> 
+
+					<br>
+					<input type="submit" value="Envoyer" class="button2" name="submit">
+				</form>
+				<!--bouton-->
+				
+				<?php     
+					if(isset($msg)){
+						echo $msg;
+					}
+				?>
+
+		    	<p>andrea.bigot974@gmail.com</p><br/>
+				<hr>
+				
+				<!-- mettre footer fusionné-->
+				<p class="footer">Site crée par Andréa BIGOT | <a href="#"> Mentions légales</a></p>
+			</div>
+		</div>
+
+</body>
+</html>
+
+
+
+
+
